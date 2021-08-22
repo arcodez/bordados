99b7666803e664b5a9834ac91fe9c0e4aa6b9044
arcodez <abelileitor123@gmail.com>
2021-08-22 16:05:50 -0400

    update navbar and add font awesome

--- components/Layout/Navbar/index.jsx
+++ components/Layout/Navbar/index.jsx
@@ -3,29 +3,23 @@ import styles from "./Navbar.module.css";
 
 export const Navbar = () => (
   <header className={styles.header}>
-    <div className="logo">Bordados Y Estampados</div>
-    <div className="nav">
-      <a href="#" className="item">
+    <div>Swicth Mode</div>
+
+    <nav>
+      <a href="#" className={styles.button}>
         Inicio
       </a>
-      <a href="#" className="item">
-        Productos
-      </a>
-      <a href="#" className="item">
-        Bordados
-      </a>
-      <a href="#" className="item">
-        Estampados
-      </a>
-      <a href="#" className="item">
-        Nosotros
-      </a>
-      <a href="#" className="item">
-        Preguntas Frecuentes
-      </a>
-      <a href="#" className="item">
-        Contacto
-      </a>
+      <a href="#">Bordados</a>
+      <a href="#">Estampados</a>
+      <i className="fas fa-tshirt "></i>
+      <a href="#">Nosotros</a>
+      <a href="#">Preguntas Frecuentes</a>
+      <a href="#">Contacto</a>
+    </nav>
+
+    <div className={styles.social}>
+      <i className="fab fa-instagram"></i>
+      <i className="fab fa-github"></i>
     </div>
   </header>
 );
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import { Fragment } from "react";
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+          integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
