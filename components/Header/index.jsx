5f03ea66860a102c624aaf5bc1bdf49e72ac09c3
arcodez <abelileitor123@gmail.com>
2021-08-23 21:08:13 +0200

    update navbar effects and add Header

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,1 +1,23 @@
-export const Header = () => <h1>Se pintan franelas</h1>;
+import { useEffect } from "react";
+import styles from "./Header.module.css";
+
+export const Header = () => {
+  return (
+    <div className={styles.header}><div className={styles.text}>
+        
+          <h1>Bordados Arcodez</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt,
+            iure. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores et culpa possimus unde dicta vel perferendis quasi architecto .
+          </p>
+    </div>
+
+      <div className={styles.box}>
+        <div className={styles.card_container}>
+          <div className={styles.card}>Primera parte</div>
+          <div className={styles.card}> Segunda Parte</div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- components/Layout/Navbar/index.jsx
+++ components/Layout/Navbar/index.jsx
@@ -1,28 +1,52 @@
 //import * as styles from "./style.js";
+import { useEffect, useState } from "react";
 import styles from "./Navbar.module.css";
 
-export const Navbar = () => (
-  <header className={styles.header}>
-    <div className={styles.change}>
-      <i className={"fas fa-toggle-off " + styles.social}></i>
-      <img src="/sun.svg" alt="" />
-    </div>
+export const Navbar = () => {
 
-    <nav>
-      <a href="#" className={styles.activo}>
-        Inicio
-      </a>
-      <a href="#">Bordados</a>
-      <a href="#">Estampados</a>
-      <i className={`fas fa-tshirt ${styles.logo}`}></i>
-      <a href="#">Nosotros</a>
-      <a href="#">Preguntas Frecuentes</a>
-      <a href="#">Contacto</a>
-    </nav>
+  const [theme, setTheme] = useState({
+    active: "off",
+    icon: "sun.svg"
+  })
 
-    <div className={styles.social}>
-      <i className="fab fa-instagram"></i>
-      <i className="fab fa-github"></i>
-    </div>
-  </header>
-);
+  function handleActivo() {
+    if (theme.active === "off") {
+      setTheme({
+        active: "on",
+        icon: "moon.svg"
+      })
+    } else {
+      setTheme({
+        active: "off",
+        icon: "sun.svg"
+      })
+    }
+  }
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.change}>
+
+        <i className={`fas fa-toggle-${theme.active} ${styles.social}`} onClick={() => handleActivo()}></i>
+        <img src={theme.icon} alt="" />
+      </div>
+
+      <nav>
+        <a href="#" className={styles.activo}>
+          Inicio
+        </a>
+        <a href="#">Bordados</a>
+        <a href="#">Estampados</a>
+        <i className={`fas fa-tshirt ${styles.logo}`}></i>
+        <a href="#">Nosotros</a>
+        <a href="#">F. A. Q</a>
+        <a href="#">Contacto</a>
+      </nav>
+
+      <div className={styles.social}>
+        <i className="fab fa-instagram"></i>
+        <i className="fab fa-github"></i>
+      </div>
+    </header>
+  )
+};
