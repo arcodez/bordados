edcacaf868684109a9b5fcba27c304f41ac977b9
arcodez <abelileitor123@gmail.com>
2021-10-24 14:18:18 -0300

    move index components

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from "next/head";
 import { Layout } from "../components/Layout";
-import { Header } from "../components/Header";
-import { Services } from "../components/Services";
-import { Carousel } from "../components/Carousel";
-import { Cards } from "../components/Cards";
+import { Header } from "../components/Index/Header";
+import { Services } from "../components/Index/Services";
+import { Carousel } from "../components/Index/Carousel";
+import { Cards } from "../components/Index/Cards";
 
 export default function Home() {
   return (
