b857be10c4122b00b69f18ff443f294e2b8f4a20
arcodez <abelileitor123@gmail.com>
2021-11-05 23:08:26 -0400

    creating pay chechkout

--- components/Payments/PaymentPage/index.jsx
+++ components/Payments/PaymentPage/index.jsx
@@ -1,12 +1,80 @@
-import React from "react";
-import { Layout as div } from "../../Layout";
+import React, { useEffect, useState } from "react";
+import { PayPalButton } from "react-paypal-button-v2";
+
+const PaypalClientId =
+  "AaYup7br6zIAPSN_UZWfEa030P8uFnsI6Y1lHwIQAyhR7QNat5URgFc5bGDBIdmIN2DE4QXmug1fqGvZ";
 
 const PaymentPage = () => {
+  const [amount, setAmount] = useState(10000);
+  const [scriptLoaded, setScriptLoaded] = useState(false);
+
+  useEffect(() => {
+    const addPaypalScript = () => {
+      const script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = "https://www.paypal.com/sdk/js?client-id=" + PaypalClientId;
+      script.async = true;
+
+      script.onload = () => {
+        setScriptLoaded(true);
+      };
+      document.body.appendChild(script);
+    };
+    addPaypalScript();
+  }, []);
+
   return (
     <div className="payment-page container">
-      <div className="client-information">Informacion del cliente </div>
-      <div className="order-total">Titulo, resumen y total de la facturas</div>
-      <div className="payment-buttons">Botones de pago</div>
+      <h2>Pagar</h2>
+
+      <div className="client-information">
+        <div className="direction">
+          <h3>Formulario para las direcciones</h3>
+        </div>
+
+        <div className="order-details">
+          <h3>Detalles del pedido</h3>
+        </div>
+      </div>
+
+      <div className="order-total">
+        <h3>Resume</h3>
+
+        <div className="amounts">
+          <div className="price-normal"> Original Price: 19,99$</div>
+          <div className="total">Total: 19,99$</div>
+        </div>
+
+        <div className="conditions">
+          Al comprar aceptas las normas Y condiciones
+        </div>
+
+        <div className="payment-buttons">
+          <div className="paypal-button">
+            {scriptLoaded && (
+              <PayPalButton
+                onSuccess={(details, data) => {
+                  console.log(
+                    "Thanks bro, for the pay" + details.payer.name.given_name
+                  );
+                  console.log(data);
+                }}
+                onError={(err) => {
+                  console.log("No tienes money we desde el error");
+                  console.log(err);
+                }}
+                amount={amount}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+
+      <style jsx>{`
+        .paypal-button {
+          width: 100%;
+        }
+      `}</style>
     </div>
   );
 };
--- pages/pay.js
+++ pages/pay.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Layout } from "../components/Layout";
 import PaymentPage from "../components/Payments/PaymentPage";
+import Head from "next/head";
 
 const PayPage = () => {
   return (
     <Layout>
+      <Head>
+        <title>Payment Page</title>
+      </Head>
       <PaymentPage />
     </Layout>
   );
